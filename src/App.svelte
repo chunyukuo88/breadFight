371e58438a16d342aa5d374b0d9536f57b47b400
chunyukuo88 <alexgochenour@gmail.com>
2024-01-05 19:01:41 -0500

    Found some boilerplate online for a 3D cube and tossed it into App.svelte after cutting out some incompatible nonsense.

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,33 @@
-<script>
-  import { onMount } from 'svelte';
-  import { Sprite } from './lib/models/Sprite';
-
-  let context;
-
-  const createAssets = () => {
-    const canvas = document.querySelector('canvas');
-    context = canvas.getContext('2d');
-    context.fillRect(0, 0, 1024, 576);
-    const player = new Sprite({
-      x: 0,
-      y: 0,
-    }, context);
-    player.draw();
-    const enemy = new Sprite({
-      x: 200,
-      y: 100
-    }, context);
-    enemy.draw();
-  };
-
-  onMount(() => {
-    createAssets();
-  });
+<script type="module">
+    import * as THREE from 'three'
+    import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+  
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+  
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+  
+    const controls = new OrbitControls(camera, renderer.domElement)
+  
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
+  
+    camera.position.z = 5
+  
+    function animate() {
+      requestAnimationFrame(animate)
+      renderer.render(scene, camera)
+      cube.rotation.x += 0.01
+      cube.rotation.y += 0.01
+    }
+    animate();
 </script>
-
-<main>
-  <canvas></canvas>
-</main>
-
-<style>
-  canvas {
-    /* height: 36rem; */
-    height: 576px;
-    /* width: 64rem; */
-    width: 1024px;
-  }
-</style>
