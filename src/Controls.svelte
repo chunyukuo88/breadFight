bf7f04f74b2a441fe0eb918811f095ef04186426
Alex Gochenour <alexgochenour@gmail.com>
2024-01-25 21:56:32 -0500

    Added controls markup.

--- src/Controls.svelte
+++ src/Controls.svelte
@@ -2,19 +2,37 @@
 
 </script>
 
-<div id="controls">Controls</div>
+<div id="controls">
+    <div id="controls__game-title">Escape From Karl Road</div>
+
+    <fieldset>
+        <legend>Picking up or dropping off?</legend>
+        <div>
+            <input name="transit" id="dropping-off" type="radio" checked>
+            <label for="dropping-off"></label>
+        </div>
+        <div>
+            <input name="transit" id="picking-up" type="radio">
+            <label for="picking-up"></label>
+        </div>
+    </fieldset>
+</div>
 
 
 <style>
     #controls {
+        background-color: red;
         display: flex;
         flex-direction: column;
-        justify-content: center;
+        justify-content: space-around;
         align-items: center;
         height: 100%;
+        padding: 0;
     }
 
-    audio {
-        width: 14em;
+    #controls__game-title {
+        background-color: blue;
+        color: white;
+        padding: 0.5rem;
     }
 </style>
